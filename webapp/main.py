27eccf9ecfec5577b149fce650a1515cd90d2425
ayulockin <mein2work@gmail.com>
2019-04-17 13:55:33 +0530

    Upload image prediction table added.
    Upload image prediction table added.

--- webapp/main.py
+++ webapp/main.py
@@ -83,6 +83,7 @@ def convertImage(imgData1, filename):
 def predict():
 
     global category_index, detectionGraph
+    predict=True
 
     filename = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
 
@@ -125,10 +126,57 @@ def predict():
 
     return(str(r))
     
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg'])
+
+@app.route("/upload", methods=['POST'])
+def upload():
+
+    upload=True
+    target = os.path.join(APP_ROOT, 'images/')
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    file = request.files['file']
+    filename = file.filename
+    dst = "/".join([target, filename])
+    file.save(dst)
+
+    img = Image.open(dst)
+    img_np = load_image_into_numpy_array(img)
+    # print(img_np.shape)
+    img_np_expanded = np.expand_dims(img_np, axis=0)
+    # print(img_np_expanded.shape)
+
+    out_dict = run_inference_onImage(img_np, detectionGraph)
+    # print(out_dict)
+    counter_dict = Counter(out_dict['detection_classes'])
+    # print(counter_dict)
+    main_dict = dict()
+
+    for ID in counter_dict.keys():
+    	indices = [i for i, x in enumerate(out_dict['detection_classes']) if x==ID]
+    	for_sum = []
+    	for index in indices:
+    		for_sum.append(out_dict['detection_scores'][index])
+    	prob = sum(for_sum)/len(out_dict['detection_scores'])
+    	main_dict[list(category_index[ID].values())[1]] = prob*100
+
+    # print(main_dict)  
+
+    sorted_dict = sorted(main_dict.items(), key=lambda x: x[1], reverse=True)
+    print(sorted_dict)
+
+    os.remove(dst)
+    r = json.dumps(sorted_dict)
+
+    return render_template('index.html', upload=upload, sorted_dict=sorted_dict)
 
 if __name__ == "__main__":
 
     port = int(os.getenv('PORT', 8000))
 
-    app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
+    #app.run(debug=False)
